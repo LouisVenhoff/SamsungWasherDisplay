82e9762088404818df487cb56b08e7e2cd5cab7a
Louis Venhoff <Louis.venhoff@gmx.net>
2025-11-16 17:07:59 +0100

    Finished setPixelMethod

--- matrixRenderer/src/lib/matrixRenderer.ts
+++ matrixRenderer/src/lib/matrixRenderer.ts
@@ -1,5 +1,7 @@
 import { Canvas, createCanvas } from "canvas";
 import { convertHexCharToBinaryMatrixChar } from "../helpers/fontParser";
+import fs from "fs";
+const hexToRgb = require("hex-to-rgb");
 
 const font = require("../../assets/font.json");
 
@@ -24,6 +26,8 @@ export class MatrixRenderer {
         //this.convertHexCharToBinaryMatrixChar(font["COLON"]);
 
         this.convertWord("W");
+        
+        this.renderWordOnCanvas();
     }
 
     private convertWord(word: string){
@@ -37,6 +41,38 @@ export class MatrixRenderer {
 
     private renderWordOnCanvas(){
         const canvas: Canvas = createCanvas(this.textSpaceInPx, this.displayHeight);
+
+        const context = canvas.getContext("2d");
+
+        const canvasData = context.createImageData(this.textSpaceInPx, this.displayHeight);
+
+        let data: Uint8ClampedArray = canvasData.data;
+
+        data = this.setPixel(data, 4, 4, "#675423");
+
+        context.putImageData(canvasData, 0, 0);
+
+        const output = fs.createWriteStream("test.png");
+        const imageStream = canvas.createPNGStream();
+
+        imageStream.pipe(output);
+        output.on("finish", () => {console.log("Ready!")});
+        
+    }
+
+    private setPixel(canvasData: any, x: number, y: number, color: string){
+        const index = (y * this.textSpaceInPx + x) * 4;
+
+        console.log(hexToRgb(color)); //[255, 255, 255];
+
+        const colorRgb: number[] = hexToRgb(color);
+
+        canvasData[index] = colorRgb[0];
+        canvasData[index + 1] = colorRgb[1];
+        canvasData[index + 2] = colorRgb[2];
+        canvasData[index + 3] = 255;
+
+        return canvasData;
     }
 
 
