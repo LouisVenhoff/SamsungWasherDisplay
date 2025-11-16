da2d4d572368a223eb49f38a4043ab061c9164e8
Louis Venhoff <Louis.venhoff@gmx.net>
2025-11-16 15:08:49 +0100

    Added renderer Class

--- matrixRenderer/src/index.ts
+++ matrixRenderer/src/index.ts
@@ -1,14 +1,17 @@
 import { RabbitReader } from "./api/rabbitmq/rabbitReader";
 import { WashingDataDto } from "./dto/washingDataDto";
 import { IMessageBrokerReader } from "./interfaces/IMessageBrokerReader";
+import { MatrixRenderer } from "./lib/matrixRenderer";
 
 async function main(){
 
-    const rabbit:IMessageBrokerReader<WashingDataDto> = new RabbitReader<WashingDataDto>();
+    // const rabbit:IMessageBrokerReader<WashingDataDto> = new RabbitReader<WashingDataDto>();
 
-    await rabbit.connect();
+    // await rabbit.connect();
 
-    await rabbit.subscribe("washerStates", (data: WashingDataDto) => {console.log(data.temp)});
+    // await rabbit.subscribe("washerStates", (data: WashingDataDto) => {console.log(data.temp)});
+
+    const renderer:MatrixRenderer = new MatrixRenderer(64, 16, 75);
 
 }
 
---
+++ matrixRenderer/src/lib/matrixRenderer.ts
@@ -0,0 +1,62 @@
+const font = require("../../assets/font.json");
+
+export class MatrixRenderer {
+
+    private displayWidth: number;
+    private displayHeight: number;
+    private textPercentage: number;
+
+    private textSpaceInPx: number;
+    private iconSpaceInPx: number;
+
+    constructor(displayWidth: number, displayHeight: number, textPercentage: number){
+        this.displayWidth = displayWidth;
+        this.displayHeight = displayHeight;
+        this.textPercentage = textPercentage;
+
+        this.textSpaceInPx = displayWidth * (textPercentage * 0.01);
+
+        this.iconSpaceInPx = this.displayWidth - this.textSpaceInPx;
+
+        let test: number = 13;
+
+        this.convertHexCharToBinaryMatrixChar(font["C"]);
+
+    }
+
+
+    private convertHexCharToBinaryMatrixChar(hexChar: string[]){
+        hexChar.forEach((char: string) => {
+            console.log(this.convertHexToBinaryString(char));
+        });
+    }
+
+    private convertHexToBinaryString(hexString: string): string{
+        
+        const resolvedNumber: number = parseInt(hexString, 16);
+
+        let binaryString: string = resolvedNumber.toString(2);
+
+        let prefixZeros: number = 0;
+
+        if(binaryString.length < 8){
+            prefixZeros = 8 - binaryString.length;
+        }
+
+        if(prefixZeros === 0){
+            return binaryString;
+        }
+
+        for(let i: number = 0; i < prefixZeros; i++){
+
+            let zero = 0;
+
+            binaryString = zero + binaryString;
+        }
+
+        return binaryString;
+    }
+
+    
+
+}
